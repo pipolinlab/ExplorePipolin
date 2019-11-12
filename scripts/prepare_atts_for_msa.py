8948d696df793c5d7ea8ae4ecd538296ada0728c
Liubov Chuprikova <chuprikovalv@gmail.com>
2019-11-12 18:57:26 +0100

    continued with the script to prepare atts for MSA

--- scripts/prepare_atts_for_msa.py
+++ scripts/prepare_atts_for_msa.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+import os
 import click
 import pandas
+from collections import namedtuple
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
 from utilities import CONTEXT_SETTINGS
+
+
+FIELDS = ('id', 'node', 'att_s', 'att_e')
+AttRegions = namedtuple('AttRegions', FIELDS, defaults=(None, ) * len(FIELDS))
 
 
 def get_att_regions(pipolins, long=False):
@@ -14,12 +22,16 @@ def get_att_regions(pipolins, long=False):
                 continue
 
         if pandas.isna(row['att3_s']) or (row['polB1_s'] > row['att1_e'] and row['polB1_e'] < row['att2_s']):
-            att_regions.append((row['att1_s'], row['att1_e']))
-            att_regions.append((row['att2_s'], row['att2_e']))
+            att_regions.append(AttRegions(id=row['id'], att_s=row['att1_s'], att_e=row['att1_e']))
+            att_regions.append(AttRegions(id=row['id'], att_s=row['att2_s'], att_e=row['att2_e']))
         else:
             # pi-polB within att2 and att3
-            att_regions.append((row['att2_s'], row['att2_e']))
-            att_regions.append((row['att3_s'], row['att3_e']))
+            att_regions.append(AttRegions(id=row['id'], att_s=row['att2_s'], att_e=row['att2_e']))
+            att_regions.append(AttRegions(id=row['id'], att_s=row['att3_s'], att_e=row['att3_e']))
+
+        if long:
+            att_regions[-2] = att_regions[-2]._replace(node=row['att1_node'])
+            att_regions[-1] = att_regions[-1]._replace(node=row['att1_node'])
 
     return att_regions
 
@@ -40,11 +52,27 @@ def prepare_atts_for_msa(ncbi_csv, new_csv):
     print(f'Number of atts from NEW_CSV file is {len(new_att_regions)}')
 
     all_att_regions = ncbi_att_regions + new_att_regions
-    lengths = [att[1] - att[0] for att in all_att_regions]
+    lengths = [att.att_e - att.att_s for att in all_att_regions]
     print(f'> Maximum att length is {max(lengths)}')
     print(f'> Minimum att length is {min(lengths)}')
 
-    # TODO: to extract att sequences I also need sequence ids!
+    genomes_dir = os.path.join(os.path.dirname(ncbi_csv), 'genomes')
+    contigs_dir = os.path.join(os.path.dirname(new_csv), 'contigs')
+    att_records = []
+    for att in all_att_regions:
+        if att.node:
+            contig_seq = SeqIO.to_dict(SeqIO.parse(os.path.join(contigs_dir, f'{att.id}.fa'), 'fasta'))[att.node]
+            record = SeqRecord(seq=contig_seq.seq[int(att.att_s) - 10:int(att.att_e) + 10],
+                               id=f'{att.id} {att.node}', description='')
+        else:
+            genome_seq = SeqIO.read(os.path.join(genomes_dir, f'{att.id}.fa'), 'fasta')
+            record = SeqRecord(seq=genome_seq.seq[int(att.att_s) - 10:int(att.att_e) + 10],
+                               id=att.id, description='')
+        att_records.append(record)
+
+    os.chdir(os.path.dirname(os.path.dirname(new_csv)))
+    with open('att_sequences.fa', 'w') as ouf:
+        SeqIO.write(att_records, ouf, 'fasta')
 
     # TODO: after extracting att sequences they need to be sorted somehow as attL and attR
 
