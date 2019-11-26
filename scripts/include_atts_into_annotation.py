bb03b4197fb9205016b6ca5dc4811fba25337024
Liubov Chuprikova <chuprikovalv@gmail.com>
2019-11-26 22:31:50 +0100

    include atts into gbk annotations

--- scripts/include_atts_into_annotation.py
+++ scripts/include_atts_into_annotation.py
@@ -2,12 +2,34 @@
 # -*- encoding: utf-8 -*-
 
 import os
+from typing import Mapping
 import click
+from random import randrange
 from Bio import SeqIO
+from Bio.SeqFeature import SeqFeature, FeatureLocation
 from utilities import CONTEXT_SETTINGS
 from utilities import Feature, Pipolin
 from utilities import read_pipolins_from_shelve
 from utilities import check_dir
+
+
+def get_bounds_with_strand(att_feature):
+    if att_feature.start < att_feature.end:
+        return (att_feature.start, att_feature.end), +1
+    else:
+        return (att_feature.end, att_feature.start), -1
+
+
+def create_att_seqfeature(att, pipolin):
+    bounds, strand = get_bounds_with_strand(att)
+    random_number = randrange(10000, 99999)
+    att_feature = SeqFeature(type='repeat_region',
+                             location=FeatureLocation(bounds[0], bounds[1], strand=strand),
+                             qualifiers={'inference': ['HMM:custom'],
+                                         'locus_tag': [f'{pipolin.strain_id}_{random_number}'],
+                                         'note': ['att repeat'],
+                                         'product': ['att repeat']})
+    return att_feature
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
@@ -20,12 +42,25 @@ def include_atts_into_annotation(shelve_file, annot_dir, new_annot_dir):
     """
     pipolins = read_pipolins_from_shelve(shelve_file)
 
-    genbank_records = {}
+    genbank_records: Mapping[str, Mapping[str, SeqIO.SeqRecord]] = {}
     for file in os.listdir(annot_dir):
         if file.endswith('.gbk'):
             # TODO: fixed LOCUS name (see extract_pipolin_regions.py)
             record = SeqIO.to_dict(SeqIO.parse(os.path.join(annot_dir, file), format='genbank'))
             genbank_records[os.path.splitext(file)[0]] = record
+
+    for pipolin in pipolins:
+        if pipolin.is_complete_genome():
+            gb_record = genbank_records[pipolin.strain_id][pipolin.strain_id]
+            for att in pipolin.atts:
+                att_feature = create_att_seqfeature(att, pipolin)
+                gb_record.features.append(att_feature)
+        else:
+            for att in pipolin.atts:
+                node = f'{att.node.split("_")[0]}_{att.node.split("_")[1]}'
+                gb_record = genbank_records[pipolin.strain_id][node]
+                att_feature = create_att_seqfeature(att, pipolin)
+                gb_record.features.append(att_feature)
 
     check_dir(new_annot_dir)
     for key, value in genbank_records.items():
