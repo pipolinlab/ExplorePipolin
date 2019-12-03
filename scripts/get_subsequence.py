31eff1d104e15f2eb0128697205d96cf645c8e2a
Liubov Chuprikova <chuprikovalv@gmail.com>
2019-12-03 16:28:39 +0100

    some polishing

--- scripts/get_subsequence.py
+++ scripts/get_subsequence.py
@@ -12,20 +12,21 @@ from utilities import CONTEXT_SETTINGS
 @click.argument('in-fasta', type=click.Path(exists=True))
 @click.argument('start', type=click.INT)
 @click.argument('end', type=click.INT)
-@click.option('--rev', is_flag=True, help='Is the subsequence on the minus strand?')
-# TODO: delete --rev, check instead if the start is greater than the end, then reverse-complement
-@click.option('--contig-name', help='If IN_FASTA is not a complete genome, but rather several contigs,'
-                                    ' specify the contig name.')
-@click.argument('out-fasta')
-def get_subsequence(in_fasta, start, end, rev, contig_name, out_fasta):
+@click.argument('out-fasta', type=click.Path())
+@click.option('--contig-name', help='If IN_FASTA is not a complete genome, but rather '
+                                    'several contigs, specify the contig name.')
+def get_subsequence(in_fasta, start, end, contig_name, out_fasta):
     """
-    This script creates an OUT_FASTA file with the subsequence that you wish to extract
-    from IN_FASTA file (a genome).
+    This script creates an OUT_FASTA file with the subsequence
+    that you wish to extract from IN_FASTA file.
+    If the START position is greater than the END position,
+    the subsequence is considered to come from the minus strand
+    and the reverse-complement will be returned.
     """
     in_seqs = SeqIO.to_dict(SeqIO.parse(in_fasta, 'fasta'))
     in_seq = in_seqs[contig_name] if contig_name else next(iter(in_seqs.values()))
 
-    subseq = in_seq.seq[start:end].reverse_complement() if rev else in_seq.seq[start:end]
+    subseq = in_seq.seq[end:start].reverse_complement() if start > end else in_seq.seq[start:end]
     header = os.path.splitext(os.path.basename(out_fasta))[0]
     record = SeqRecord(seq=subseq, id=header, description='')
 
