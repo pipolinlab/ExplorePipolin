9c7d3b31284a75f53526c8b4fd3df326b1ce2624
Liubov Chuprikova <chuprikovalv@gmail.com>
2020-02-25 16:19:03 +0100

    color amr and virulence genes using abricate results

--- scripts/easyfig_add_colours.py
+++ scripts/easyfig_add_colours.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+import os
 import click
 from utilities import CONTEXT_SETTINGS, GenBankRecords
 from utilities import read_genbank_records, write_genbank_records
+from Bio.SeqIO import SeqRecord
 
 red = '255 0 0'   # Primer-independent DNA polymerase PolB
 brick_red = '139 58 58'   # Tyrosine recombinase XerC
@@ -34,6 +36,9 @@ products_to_colours = {'Primer-independent DNA polymerase PolB': red,
                        'paired-ends': pink, 'Prophage integrase IntS': brown,
                        'other': floral_white}
 
+VIRULENCE_DBs = ['vfdb', 'ecoli_vf', 'ecoli_virfinder']
+AMR_DBs = ['megares_noSNPs', 'argannot', 'ncbi', 'card', 'resfinder']
+
 
 def colour_feature(qualifiers):
     if 'product' in qualifiers:
@@ -60,14 +65,84 @@ def add_colours(gb_records: GenBankRecords):
                     colour_feature(feature.qualifiers)
 
 
+def read_record_ranges(record: SeqRecord):
+    ranges = []
+    for feature in record.features:
+        ranges.append((feature.location.start, feature.location.end, feature.location.strand))
+    return ranges
+
+
+def find_feature_position(s_ranges, q_range):
+    all_ranges = s_ranges + [q_range]
+    all_ranges.sort(key=lambda x: (x[0], x[1]))
+    q_position = all_ranges.index(q_range)
+    q = set(range(q_range[0], q_range[1] + 1))
+    if all_ranges[q_position][0] < all_ranges[q_position - 1][1]:
+        if all_ranges[q_position][2] == all_ranges[q_position - 1][2]:
+            s = set(range(all_ranges[q_position - 1][0], all_ranges[q_position - 1][1]))
+            qs = q.intersection(s)
+            # if len(qs) / len(s) >= 0.9:
+            s_position = s_ranges.index(all_ranges[q_position - 1])
+            return s_position
+    if all_ranges[q_position][1] > all_ranges[q_position + 1][0]:
+        if all_ranges[q_position][2] == all_ranges[q_position + 1][2]:
+            s = set(range(all_ranges[q_position + 1][0], all_ranges[q_position + 1][1]))
+            qs = q.intersection(s)
+            # if len(qs) / len(s) >= 0.9:
+            s_position = s_ranges.index(all_ranges[q_position + 1])
+            return s_position
+    return None
+
+
+def color_feature(record: SeqRecord, feature_position, db_type):
+    if record.features[feature_position].qualifiers['colour'] == ['255 250 240']:
+        if db_type == 'vir':
+            record.features[feature_position].qualifiers['colour'] = orange
+        elif db_type == 'amr':
+            record.features[feature_position].qualifiers['colour'] = light_steel_blue
+        else:
+            raise AssertionError('Something is wrong here!')
+
+
+def add_info_from_summary(gb_records: GenBankRecords, summary_path, db_type):
+    with open(summary_path) as inf:
+        _ = inf.readline()   # skip header
+        for line in inf:
+            entry_line = line.strip().split(sep='\t')
+            q_id = entry_line[1]
+            q_location = (int(entry_line[2]), int(entry_line[3]), 1 if entry_line[4] == '+' else -1)
+            q_cov = float(entry_line[10])
+            if q_cov >= 10.0:
+                record = gb_records[os.path.basename(summary_path)[:-4]][q_id]
+                record_ranges = read_record_ranges(record)
+                feature_position = find_feature_position(record_ranges, q_location)
+                if feature_position is not None:
+                    color_feature(record, feature_position, db_type)
+                gb_records[os.path.basename(summary_path)[:-4]][q_id] = record
+
+
+def find_and_color_amr_and_virulence(gb_records: GenBankRecords, abricate_dir):
+    contents = os.listdir(abricate_dir)
+    for content in contents:
+        if content in VIRULENCE_DBs or content in AMR_DBs:
+            content_path = os.path.join(abricate_dir, content)
+            db_type = 'vir' if content in VIRULENCE_DBs else 'amr'
+            summaries = os.listdir(content_path)
+            for summary in summaries:
+                add_info_from_summary(gb_records, os.path.join(content_path, summary), db_type)
+
+
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('in-dir', type=click.Path(exists=True))
-def easyfig_add_colours(in_dir):
+@click.argument('abricate_dir', type=click.Path(exists=True), required=False)
+def easyfig_add_colours(in_dir, abricate_dir):
     """
     IN_DIR contains *.gbk files to modify.
     """
     gb_records = read_genbank_records(in_dir)
     add_colours(gb_records)
+    if abricate_dir is not None:
+        find_and_color_amr_and_virulence(gb_records, abricate_dir)
     write_genbank_records(gb_records, in_dir)
 
 
