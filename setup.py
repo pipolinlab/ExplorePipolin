1a633ab83a65e01d17e16c1404e67fe72693cd6f
Liubov Chuprikova <chuprikovalv@gmail.com>
2020-04-23 17:39:59 +0200

    edit pi-polB file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     install_requires=['click', 'Biopython', 'bcbio-gff', 'prefect'],
     python_requires='~=3.7',
     package_data={
-        'pipolin_finder': ['data/attL.fa', 'data/HHpred_proteins.faa', 'data/pi-polB.fa']
+        'pipolin_finder': ['data/attL.fa', 'data/HHpred_proteins.faa', 'data/pi-polB.faa']
     },
     entry_points={
         'console_scripts': ['find_pipolins=pipolin_finder.main:explore_pipolins']
