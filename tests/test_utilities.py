47c7abf2386d6c650d1c7e380273575e7069f3cd
Liubov Chuprikova <chuprikovalv@gmail.com>
2020-05-04 10:51:14 +0200

    started tests

---
+++ tests/test_utilities.py
@@ -0,0 +1,20 @@
+import unittest
+
+from pipolin_finder.utilities import Orientation, Contig
+
+
+class UtilitiesTestCase(unittest.TestCase):
+    def test_orientation(self):
+        self.assertEqual(-Orientation.FORWARD, Orientation.REVERSE)
+        self.assertEqual(-Orientation.REVERSE, Orientation.FORWARD)
+
+    def test_default_contig_orientation(self):
+        self.assertEqual(Contig('foo', 10).contig_orientation, Orientation.FORWARD)
+
+    def test_blast_orientation(self):
+        self.assertEqual(Orientation.orientation_from_blast(1), Orientation.FORWARD)
+        self.assertEqual(Orientation.orientation_from_blast(-1), Orientation.REVERSE)
+
+
+if __name__ == '__main__':
+    unittest.main()
