58c59fa8d8bc8fdd5b938715566027149d60239b
Liubov Chuprikova <chuprikovalv@gmail.com>
2020-05-05 20:30:45 +0200

    "reverse" atts_denovo, when it is required

--- pipolin_finder/identify_pipolins_roughly.py
+++ pipolin_finder/identify_pipolins_roughly.py
@@ -136,7 +136,7 @@ def are_polbs_present(gquery: GQuery):
 
 
 @task(skip_on_upstream_skip=False)
-def are_atts_present(gquery):
+def are_atts_present(gquery: GQuery):
     logger = prefect.context.get('logger')
 
     if len(gquery.atts) == 0 and len(gquery.denovo_atts) == 0:
@@ -148,7 +148,16 @@ def are_atts_present(gquery):
         logger.warning(f'\n\n>>>No "usual" atts were found, but some atts were found by denovo search!'
                        f'For more details, check the {gquery.gquery_id}.atts file in the atts_denovo directory!\n')
         # TODO: check that it's only one repeat! Although, this shouldn't be a problem.
-        gquery.atts.extend(gquery.denovo_atts)
+        atts_frames = [att.frame for att in gquery.denovo_atts]
+        if len(set(atts_frames)) != 1:
+            raise AssertionError('ATTs are expected to be in the same frame, as they are direct repeats!')
+        if set(atts_frames).pop() == gquery.target_trnas_denovo[0].frame:
+            reverse_denovo_atts = []
+            for att in gquery.denovo_atts:
+                reverse_denovo_atts.append(Feature(start=att.start, end=att.end, frame=-att.frame, contig=att.contig))
+                gquery.atts.extend(reverse_denovo_atts)
+        else:
+            gquery.atts.extend(gquery.denovo_atts)
         gquery.target_trnas.extend(gquery.target_trnas_denovo)
 
     elif len(gquery.denovo_atts) != 0:
