13d4411d84c25de7abb5c2086b8a9689a0e5a8d8
Liubov Chuprikova <chuprikovalv@gmail.com>
2020-06-23 16:02:43 +0200

    prefect>=0.11.5 (with case); biopython<=1.76 (1.77 incompatible with bcbio-gff)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='ExplorePipolin',
     version='0.0.1',
     packages=setuptools.find_packages(),
-    install_requires=['click', 'biopython', 'bcbio-gff', 'prefect'],
+    install_requires=['click', 'biopython <= 1.76', 'bcbio-gff', 'prefect >= 0.11.5'],
     python_requires='>=3.6',
     package_data={
         'explore_pipolin': ['data/attL.fa', 'data/HHpred_proteins.faa', 'data/pi-polB.faa']
