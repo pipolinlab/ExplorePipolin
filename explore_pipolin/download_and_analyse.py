4ba664caddc8276be1cc5315426112c6355e48f1
liubov <chuprikovalv@gmail.com>
2021-03-25 17:01:43 +0100

    working with a single xml from ENA db

--- explore_pipolin/download_and_analyse.py
+++ explore_pipolin/download_and_analyse.py
@@ -1,11 +1,9 @@
 import gzip
 import os
 import shutil
-import sys
 
-import requests
 import urllib.request
-import json
+from urllib.error import URLError
 import xml.etree.ElementTree as XMLParser
 
 import click
@@ -15,31 +13,22 @@ from explore_pipolin.common import CONTEXT_SETTINGS
 from explore_pipolin.utilities.io import create_seqio_records_dict
 
 
-def yield_accessions(json_file):
-    with open(json_file) as inf:
-        content = inf.readline()
-    for entry in json.loads(content):
-        yield entry['assembly_accession']
+def yield_acc_and_url(ena_xml_file):
+    for event, elem in XMLParser.iterparse(ena_xml_file, events=('start',)):
+        accession, url = None, None
+        if event == 'start' and elem.tag == 'ASSEMBLY':
+            accession = elem.attrib['accession']
 
+            if elem.find('ASSEMBLY_LINKS') is not None:
+                for link in elem.find('ASSEMBLY_LINKS').findall('ASSEMBLY_LINK'):
+                    url_link = link.find('URL_LINK')
+                    if url_link is not None:
+                        if url_link.find('LABEL') is not None and url_link.find('LABEL').text == 'WGS_SET_FASTA':
+                            if url_link.find('URL') is not None:
+                                url = url_link.find('URL').text
 
-def retrieve_ena_data(acc):
-    server = "https://www.ebi.ac.uk/ena/browser/api/xml/" + acc
-    r = requests.get(server)
-
-    if not r.ok:
-        r.raise_for_status()
-        sys.exit()
-
-    return r.text
-
-
-def get_fasta_url(ena_data):
-    root = XMLParser.fromstring(ena_data)
-    for ass in root.findall('ASSEMBLY'):
-        for link in ass.find('ASSEMBLY_LINKS').findall('ASSEMBLY_LINK'):
-            url_link = link.find('URL_LINK')
-            if url_link.find('LABEL').text == 'WGS_SET_FASTA':
-                return url_link.find('URL').text
+        if accession is not None and url is not None:
+            yield accession, url
 
 
 def retrieve_fasta_file(fasta_url, file_path):
@@ -83,30 +72,34 @@ def _update_analysed(acc, output_dir):
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.argument('metadata-file', type=click.Path(exists=True))
+@click.argument('ena-xml-file', type=click.Path(exists=True))
 @click.argument('output-dir', type=click.Path())
 @click.option('--force', is_flag=True,
               help='If the output directory already exists, erase its content before starting the analysis.')
-def download_and_analyse(metadata_file, output_dir, force):
+def download_and_analyse(ena_xml_file, output_dir, force):
     if force:
         os.remove(output_dir)
     os.makedirs(output_dir, exist_ok=True)
 
-    for acc in yield_accessions(metadata_file):
-        print(acc)
+    for acc, url in yield_acc_and_url(ena_xml_file):
+        print(acc, url)
 
         if _is_analysed(acc, output_dir):
             continue
 
-        ena_data = retrieve_ena_data(acc)
-        fasta_url = get_fasta_url(ena_data)
-        if fasta_url is not None:
-            file_path = os.path.join(output_dir, acc + '.fasta.gz')
-            retrieve_fasta_file(fasta_url, file_path)
-            file_to_analyse = unzip_fasta_file(file_path)
-            change_fasta_identifiers(file_to_analyse)
-            os.system(f'explore_pipolin --out-dir {output_dir} --add-colours {file_to_analyse}')
-            os.remove(file_to_analyse)
+        file_path = os.path.join(output_dir, acc + '.fasta.gz')
+
+        try:
+            retrieve_fasta_file(url, file_path)
+        except URLError:
+            if not _is_analysed(acc, output_dir):
+                _update_analysed(acc, output_dir)
+            continue
+
+        file_to_analyse = unzip_fasta_file(file_path)
+        change_fasta_identifiers(file_to_analyse)
+        os.system(f'explore_pipolin --out-dir {output_dir} --add-colours {file_to_analyse}')
+        os.remove(file_to_analyse)
 
         _update_analysed(acc, output_dir)
 
